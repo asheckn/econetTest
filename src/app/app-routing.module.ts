f36e152946210d2ef238541e060ee194d09d2008
Nyasha Asheck Hove <50446026+asheckn@users.noreply.github.com>
2024-05-30 16:33:03 +0200

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Views/login/login.component';
+import { DashboardComponent } from './Views/dashboard/dashboard.component';
+import { ProjectsComponent } from './Views/projects/projects.component';
+import { MembersComponent } from './Views/members/members.component';
+import { ProjectComponent } from './Components/project/project.component';
+import { CreateProjectComponent } from './Views/create-project/create-project.component';
+import { ProjectDetailComponent } from './Views/project-detail/project-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component: LoginComponent},
+  {path: "dashboard", component: DashboardComponent, children: [
+    {path: "projects", component: ProjectsComponent},
+    {path: "members", component: MembersComponent},
+    {path: "create-Project", component: CreateProjectComponent},
+    {path: "project-detail", component: ProjectDetailComponent},
+  ]},
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './Views/dashboard/dashboard.component';
+import { LoginComponent } from './Views/login/login.component';
+import { ProjectsComponent } from './Views/projects/projects.component';
+import { MembersComponent } from './Views/members/members.component';
+import { TasksComponent } from './Views/tasks/tasks.component';
+import { TaskComponent } from './Components/task/task.component';
+import { ProjectComponent } from './Components/project/project.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { CreateProjectComponent } from './Views/create-project/create-project.component';
+import { ProjectDetailComponent } from './Views/project-detail/project-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    ProjectsComponent,
+    MembersComponent,
+    TasksComponent,
+    TaskComponent,
+    ProjectComponent,
+    NavBarComponent,
+    CreateProjectComponent,
+    ProjectDetailComponent
   ],
   imports: [
     BrowserModule,
